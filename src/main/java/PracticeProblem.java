f69b46a30ca3429434e88598f9cb43f1a5019a84
Joe-Yang1 <348993072@tdsb.ca>
2026-05-04 15:28:39 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
+/**
+
+        * File: 
+
+        * Author: Assumptions
+
+        * Date Created: April 20, 2026
+
+        * Date Last Modified: April 20, 2026
+
+        */
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
 
+	public static boolean hasCapital(String word){
+
+		for (int i = 0; i < word.length(); i++) {
+			if (Character.isUpperCase(word.charAt(i))){
+				return true;
+			}
+			
+		}
+
+		return false;
 	}
+
+	public static boolean isPrime(int num){
+		
+		if (num <= 1){
+			return false;
+		}
+
+		for (int i = 2; i < num; i++) {
+		if (num % i == 0){
+			return false;
+		}
+	}
+	return true;
+}
 
 }
